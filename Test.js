af28b0165f11386c4819455d581fc541aa1b54c1
LevPelihov <levpelihov@gmail.com>
2024-11-21 10:25:57 +0300

    Первый комит

---
+++ Test.js
@@ -0,0 +1,15 @@
+function generateShape(integer){
+  let num = '+', res = '+';
+  for(let i=1; i < integer * integer; i++) {
+    if(i % integer == 0 && integer *integer != i ) {
+      res += '\n';
+      
+    }
+    res += num;
+    
+    }
+   
+   
+   return res;
+}
+console.log(generateShape(5));
